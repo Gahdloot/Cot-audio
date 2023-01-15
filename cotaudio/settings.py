3a35bb5e3095ecdd725725adcd66f43f656eaa90
Michael David <davidlearn27@gmail.com>
2023-01-15 06:31:37 +0100

    Modified files for heroku deployment (3)

--- cotaudio/settings.py
+++ cotaudio/settings.py
@@ -131,7 +131,7 @@ STATIC_URL = 'static/'
 #STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
-django_heroku.setings(locals())
+django_heroku.settings(locals())
 
 #MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
